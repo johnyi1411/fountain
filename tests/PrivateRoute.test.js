4905c5fe22e83724f4799663b6384044c9aaf176
John Yi <john.yi1411@gmail.com>
2019-10-05 10:53:53 -0700

    Create unit tests for App and PrivateRoute

--- tests/PrivateRoute.test.js
+++ tests/PrivateRoute.test.js
@@ -1,19 +1,16 @@
 /* eslint-disable react/jsx-filename-extension */
 /* eslint-disable no-undef */
 import React from 'react';
-import Enzyme, { shallow, mount } from 'enzyme';
+import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import { Route, Router } from 'react-router-dom';
-import { createMemoryHistory } from 'history';
-import App from '../src/components/App';
+import { Route } from 'react-router-dom';
 import PrivateRoute from '../src/components/PrivateRoute';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('App', () => {
   it('renders Routes and Private Routes', () => {
-    const wrapper = shallow(<App />);
-    expect(wrapper.find(Route)).toHaveLength(2);
-    expect(wrapper.find(PrivateRoute)).toHaveLength(2);
+    const wrapper = shallow(<PrivateRoute />);
+    expect(wrapper.find(Route)).toHaveLength(1);
   });
 });
