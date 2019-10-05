0f0e6e85bb54f25c917211f9bf40716947f8c6c2
John Yi <john.yi1411@gmail.com>
2019-10-04 20:52:06 -0700

    Implement react router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import {
+  BrowserRouter as Router, Switch, Route, Link,
+} from 'react-router-dom';
 import Login from './Login';
 
 class App extends React.Component {
@@ -7,20 +10,48 @@ class App extends React.Component {
     this.state = {
       jobs: [],
       user: null,
+      employer: false,
     };
     this.handleUserChange = this.handleUserChange.bind(this);
   }
 
   handleUserChange(user) {
-    this.setState({ user: user || null });
+    this.setState({ user });
   }
 
   render() {
     const { user } = this.state;
-    const login = user ? <button type="submit" onClick={this.handleUserChange}>Logout</button> : <Login handleUserChange={this.handleUserChange} />;
+    const login = user ? (
+      <button
+        type="submit"
+        onClick={() => {
+          this.handleUserChange(null);
+        }}
+      >
+      Logout
+      </button>
+    ) : <Login handleUserChange={this.handleUserChange} />;
     return (
       <div>
         {login}
+        <Router>
+          <div>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/topics">Topics</Link>
+              </li>
+            </ul>
+            <Route exact path="/" render={() => <h1>just /</h1>} />
+            <Route path="/about" render={() => <h1>about</h1>} />
+            <Route path="/topics" render={() => <h1>topics</h1>} />
+          </div>
+        </Router>
       </div>
     );
   }
