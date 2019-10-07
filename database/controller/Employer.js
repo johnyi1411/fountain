e1bd4607a9f314f6769724b5f63a165531f8cba0
John Yi <john.yi1411@gmail.com>
2019-10-06 22:02:37 -0700

    Create controller to interact with postgres db for employer table. Set up server to listen to post requestes at /employer.

---
+++ database/controller/Employer.js
@@ -0,0 +1,16 @@
+const { Employer } = require('../model/Models');
+
+module.exports = {
+  createEmployer: (email, password, company, callback) => {
+    Employer.create({ email, password, company })
+      .then((employer) => {
+        console.log(employer.id);
+        callback(null, employer);
+      })
+      .catch((err) => {
+        console.log('error creating employer');
+        console.log(err);
+        callback(err);
+      });
+  },
+};
--- server/server.js
+++ server/server.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const EmployerController = require('../database/controller/Employer');
 
 const port = 3000;
 
 const app = express();
 
 app.use(morgan('tiny'));
+app.use(bodyParser.json());
 app.use(express.static('public'));
 
 app.listen(port);
+
+app.post('/employer', (req, res) => {
+  const { email, password, company } = req.body;
+  EmployerController.createEmployer(email, password, company, (err, result) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log(result);
+    res.send(result);
+  });
+});
