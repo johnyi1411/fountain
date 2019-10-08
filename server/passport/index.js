a30090af15c85bf92014ccabe95354cf5b0d125e
John Yi <john.yi1411@gmail.com>
2019-10-08 00:19:58 -0700

    Assign userType to passport.user that defines the session user as employer or applicant. Conditionally deserialize from applicant or employer table based off of this property.

--- server/passport/index.js
+++ server/passport/index.js
@@ -1,30 +1,50 @@
 const passport = require('passport');
 const LocalStrategy = require('./LocalStrategy');
 const Employer = require('../../database/model/Employer');
+const Applicant = require('../../database/model/Applicant');
 
 // called on login, saves the id to session req.session.passport.user = {id:'..'}
 passport.serializeUser((user, done) => {
   console.log('*** serializeUser called, user: ');
   console.log(user); // the whole raw user object!
   console.log('---------');
-  done(null, user.dataValues.employer_id || user.dataValues.applicant_id);
+  done(null, {
+    id: user.dataValues.employer_id || user.dataValues.applicant_id,
+    userType: user.dataValues.employer_id ? 'employer' : 'applicant',
+  });
 });
 
 // user object attaches to the request as req.user
-passport.deserializeUser((id, done) => {
+passport.deserializeUser((user, done) => {
   console.log('DeserializeUser called');
-  console.log(id);
-  Employer.findOne({ where: { employer_id: id } })
-    .then((employer) => {
-      console.log('*** Deserialize user, user:');
-      console.log(employer);
-      console.log('--------------');
-      done(null, employer);
-    })
-    .catch((error) => {
-      console.log('deserialize error:');
-      console.log(error);
-    });
+  console.log(user);
+  if (user.userType === 'employer') {
+    Employer.findOne({ where: { employer_id: user.id } })
+      .then((employer) => {
+        console.log('*** Deserialize user, user:');
+        console.log(employer);
+        console.log('--------------');
+        done(null, employer);
+      })
+      .catch((error) => {
+        console.log('deserialize error:');
+        console.log(error);
+      });
+  }
+
+  if (user.userType === 'applicant') {
+    Applicant.findOne({ where: { applicant_id: user.id } })
+      .then((applicant) => {
+        console.log('*** Deserialize user, user:');
+        console.log(applicant);
+        console.log('--------------');
+        done(null, applicant);
+      })
+      .catch((error) => {
+        console.log('deserialize error:');
+        console.log(error);
+      });
+  }
 });
 
 //  Use Strategies
